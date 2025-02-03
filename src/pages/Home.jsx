722f0df17f7ff570f2774bc337c5f778ebfcab99
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-02-03 09:29:58 +0100

    Prepare the Create Employee Form in React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import CreateEmployeeForm from "../components/CreateEmployeeForm"
 
 const Home = () => {
      return (
-        <>
+        <section>
             <h1>HRnet</h1>
             <Link to="/employee-list">View Current Employees</Link>
             <h2>Create Employee</h2>
             <CreateEmployeeForm />
-        </>
+        </section>
      )
 }
 
